c1126462175bf18e2093b6b12c4e74dc8c35645b
Marta <lagiewka.marta.dev@gmail.com>
2019-09-10 07:35:41 +0200

    FINISH first version of autocompleting functionality in AutocompleteInput.

--- src/Components/Autocomplete/AutocompleteInput.js
+++ src/Components/Autocomplete/AutocompleteInput.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import useInputState from '../../hooks/useInputState';
+import useSuggestions from '../../hooks/useSuggestions';
+import { useEffect, useState } from 'react';
 
-export default function AutocompleteInput(props) {
-	const [InputValue, updateInputValue, resetInputValue] = useInputState('', "inputValCity");
+export default function AutocompleteInput({ availableCountries }) {
+	const [InputValue, setInputValue, updateInputValue] = useInputState('', 'inputValCity');
+	const [suggestions, setSuggestions, updateSuggestions] = useSuggestions([], InputValue, availableCountries);
+	const [isShowingSuggestions, setIsShowingSuggestions] = useState(false);
+
+	useEffect(() => {
+		updateSuggestions(InputValue);
+	}, [InputValue]);
+
+	const selectSuggestion = suggestion => {
+		setInputValue(suggestion);
+		setSuggestions([]);
+		setIsShowingSuggestions(false);
+	};
+
+	const renderSuggestions = () => {
+		if (suggestions.length !== 0 && isShowingSuggestions) {
+			return (
+				<ul>
+					{suggestions.map((el, idx) => (
+						<li onClick={() => selectSuggestion(el)} key={idx}>
+							{' '}
+							{el}
+						</li>
+					))}
+				</ul>
+			);
+		}
+		return null;
+	};
+
 	return (
 		<div className="Autocomplete">
 			<form>
-				<input type="text" value={InputValue} onChange={updateInputValue} />
+				<input
+					type="text"
+					value={InputValue}
+					onChange={updateInputValue}
+					onFocus={() => setIsShowingSuggestions(true)}
+					placeholder="Enter country name"
+				/>
 				<button type="submit">search</button>
+				{renderSuggestions()}
 			</form>
 		</div>
 	);
